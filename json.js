c76b96bb657b12ba23e671f6d521fe9c31e61390
slam814 <abdussalam6111997@gmail.com>
2024-05-05 06:56:05 +0600

    stingify file added

---
+++ json.js
@@ -0,0 +1,7 @@
+const user = { id: 1, name: 'gorib Amir', job: 'Actor'};
+//JvavScript Object Notetion (JSON)
+// JSON.stringify();
+
+const stringifyed = JSON.stringify(user);
+
+console.log(user);
